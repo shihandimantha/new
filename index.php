d261083aa720c8349811dc13f38143517109324c
shihandimantha <76395781+shihandimantha@users.noreply.github.com>
2021-09-13 11:00:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peace Day 2021</title>
+    <link rel="shortcut icon" href="image/logo.jpg" type="image/x-icon">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- gradeint background  -->
+
+<div class="gradient-background"></div> 
+
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-dove"></i> PEACE DAY 2021 </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#Activities">Activities</a>
+        <a href="#Certification">Certification</a>
+        <a href="#Signup/Login">Signup/Login</a>
+        
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3 class="glow">9 DAYS MORE</h3>
+
+        <p>Recovering better for an equitable and sustainable world</p>
+        
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- courses section starts  -->
+
+<section class="Activities" id="Activities">
+
+    <h1 class="heading"> <span>Activities</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <a href="index2.php"><img src="image/poster.png" alt=""></a>
+            <h3>Poster Competetion</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            
+            
+        </div>
+
+        <div class="box">
+            <img src="image/course-2.svg" alt="">
+            <h3>seo marketing</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            <a href="#" class="btn">read more</a>
+            <div class="icons">
+                <a href="#"> <i class="fas fa-book"></i> 5 modules </a>
+                <a href="#"> <i class="fas fa-clock"></i> 6 hours </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/course-3.svg" alt="">
+            <h3>science and technology</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            <a href="#" class="btn">read more</a>
+            <div class="icons">
+                <a href="#"> <i class="fas fa-book"></i> 5 modules </a>
+                <a href="#"> <i class="fas fa-clock"></i> 6 hours </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/course-4.svg" alt="">
+            <h3>music and dancing</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            <a href="#" class="btn">read more</a>
+            <div class="icons">
+                <a href="#"> <i class="fas fa-book"></i> 5 modules </a>
+                <a href="#"> <i class="fas fa-clock"></i> 6 hours </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/course-5.svg" alt="">
+            <h3>teaching and staff</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            <a href="#" class="btn">read more</a>
+            <div class="icons">
+                <a href="#"> <i class="fas fa-book"></i> 5 modules </a>
+                <a href="#"> <i class="fas fa-clock"></i> 6 hours </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/course-6.svg" alt="">
+            <h3>graphic designing</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ex?</p>
+            <a href="#" class="btn">read more</a>
+            <div class="icons">
+                <a href="#"> <i class="fas fa-book"></i> 5 modules </a>
+                <a href="#"> <i class="fas fa-clock"></i> 6 hours </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- courses section ends -->
+
+<!-- Certification section starts  -->
+
+<section class="Certification" id="Certification">
+
+    <h1 class="heading"> <span>our Certification</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/teacher-1.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/teacher-2.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/teacher-3.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/teacher-4.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/teacher-5.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/teacher-6.png" alt="">
+            <h3>john deo</h3>
+            <span>online teacher</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- Certification section ends -->
+
+<!-- Signup/Login section starts  -->
+
+<section class="Signup/Login" id="Signup/Login">
+
+    <h1 class="heading"> <span>our Signup/Logins</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-paper-Signup/Logine"></i>
+            <h3>basic Signup/Login</h3>
+            <ul>
+                <li>full courses</li>
+                <li>projects</li>
+                <li>certification</li>
+                <li>job placements</li>
+                <li>24/7 support</li>
+            </ul>
+            <a href="#"> choose Signup/Login</a>
+            <div class="price"> <span>$</span>30<span>/mo</span> </div>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-Signup/Logine"></i>
+            <h3>standard Signup/Login</h3>
+            <ul>
+                <li>full courses</li>
+                <li>projects</li>
+                <li>certification</li>
+                <li>job placements</li>
+                <li>24/7 support</li>
+            </ul>
+            <a href="#"> choose Signup/Login</a>
+            <div class="price"> <span>$</span>60<span>/mo</span> </div>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-rocket"></i>
+            <h3>premium Signup/Login</h3>
+            <ul>
+                <li>full courses</li>
+                <li>projects</li>
+                <li>certification</li>
+                <li>job placements</li>
+                <li>24/7 support</li>
+            </ul>
+            <a href="#"> choose Signup/Login</a>
+            <div class="price"> <span>$</span>90<span>/mo</span> </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- Signup/Login section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom javascript file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
